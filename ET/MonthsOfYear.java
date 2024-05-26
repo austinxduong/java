dbf7c6c592907b759f2bf33168e174638256735e
austinxduong <austinxduong@gmail.com>
2024-05-26 14:12:00 -0700

    months of year

---
+++ ET/MonthsOfYear.java
@@ -0,0 +1,23 @@
+package ET;
+
+public class MonthsOfYear {
+    public static String monthName(int num) {
+        String[] array = 
+        {
+            "Months",
+		    "January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+        };
+        return array[num];
+    }
+}
