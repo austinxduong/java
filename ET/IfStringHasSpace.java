66b524887243244df790f4d4282d4bb669d7e815
austinxduong <austinxduong@gmail.com>
2024-05-31 13:50:49 -0700

    string has space

---
+++ ET/IfStringHasSpace.java
@@ -0,0 +1,11 @@
+package ET;
+
+public class IfStringHasSpace {
+    public static boolean stringHasSpace(String str) {
+        if(str.contains(" ")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ ET/StringEndsWithS.java
@@ -0,0 +1,7 @@
+package ET;
+
+public class StringEndsWithS{
+    public static boolean StringS(String word) {
+        return word.endsWith("s");
+    }
+}
