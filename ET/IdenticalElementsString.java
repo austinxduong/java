20a6b8173892385a77e68b11839a6d65e8fc2ae7
austinxduong <austinxduong@gmail.com>
2024-06-01 14:14:42 -0700

    identical elements loop string

---
+++ ET/IdenticalElementsString.java
@@ -0,0 +1,13 @@
+package ET;
+
+public class IdenticalElementsString {
+    public static boolean isIdentical(String str) {
+        for(int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != str.charAt(0)) {
+				return false;
+			}
+		}
+		return true;
+  }
+}
+// loops through a string, and returns true/false of all elements within the string are identical
---
+++ ET/ObjectComparison.java
@@ -0,0 +1,7 @@
+package ET;
+
+public class ObjectComparison {
+   public static boolean ObjComparison(Object firObject, Object secObject) {
+    return firObject == null || secObject == null || !firObject.equals(secObject) ? false : true;
+   } 
+}
