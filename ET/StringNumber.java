3763e7a921ac4f84f75e633c22886385e705bd08
austinxduong <austinxduong@gmail.com>
2024-06-01 12:58:38 -0700

    string/integer

---
+++ ET/StringNumber.java
@@ -0,0 +1,12 @@
+package ET;
+
+public class StringNumber{
+    public static int toInteger(String str) { // computes a string to an integer,                                     // and this particular method with throw a NumberFormatException
+        return Integer.parseInt(str);
+    }
+
+    public static String toString(int num){ // computes an integer to a string
+        return String.valueOf(num);
+    }
+}
+
