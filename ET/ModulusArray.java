a9243fef2fed3a4770d46ec8c9290e7a952a3fb6
austinxduong <austinxduong@gmail.com>
2024-06-03 12:46:56 -0700

    modulus array

---
+++ ET/ModulusArray.java
@@ -0,0 +1,11 @@
+package ET;
+
+public class ModulusArray {
+    public static int[] modulus(int[] arr, int num) {
+        int[] modulusArray = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            modulusArray[i] = arr[i] % num;
+        }
+        return modulusArray;
+    }
+}
