d67a6b8652886102b2053330d262d93014bd6aba
austinxduong <austinxduong@gmail.com>
2024-06-02 22:13:11 -0700

    str compare

---
+++ ET/StringCompare.java
@@ -0,0 +1,11 @@
+package ET;
+
+public class StringCompare {
+    public static boolean strCom(String str1, String str2) {
+        if (!str1.equalsIgnoreCase(str2)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
