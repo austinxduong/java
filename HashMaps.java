1564c62d40f2af017590059483dae24dfdd0fd36
austinxduong <austinxduong@gmail.com>
2024-06-03 19:38:49 -0700

    hash map character count

--- HashMaps.java
+++ HashMaps.java
@@ -15,15 +15,41 @@ public class HashMaps{
                 charCountMap.put(c, 1);
             }
         }
-        
-
         for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
+            System.out.println(entry.getKey()+ ":" + " " + entry.getValue());
         }
     }
-
     public static void main(String[] args) {
-        String str = "Howdy! Je suis Austin. Cheese is delightful";
+        String str = "Howdy! Je suis Austin. Gruyère cheese is delelectably delightful";
         characterCount(str);
     }
 }
+
+/*
+A: 1
+G: 1
+H: 1
+J: 1
+ : 8
+!: 1
+a: 1
+b: 1
+c: 2
+d: 3
+e: 9
+f: 1
+g: 1
+è: 1
+h: 2
+i: 4
+l: 5
+n: 1
+.: 1
+o: 1
+r: 2
+s: 5
+t: 3
+u: 4
+w: 1
+y: 3
+ */
