46321e9c7a34ccfcc6ed1e077068c9c61ebaca07
austinxduong <austinxduong@gmail.com>
2024-06-04 17:44:07 -0700

    string inverse/capitalize

---
+++ ET/StringInverseCapitalize.java
@@ -0,0 +1,14 @@
+package ET;
+
+public class StringInverseCapitalize {
+    public static String strInverse(String str) {
+        String capitalize = str.toUpperCase();
+        String strCapitalize = "";
+
+        for (int i = 0; i < capitalize.length(); i++) {
+            char c = capitalize.charAt(i);
+            strCapitalize = c + strCapitalize;
+        }
+        return strCapitalize;
+    }
+}
