91c87598d0144521e850959ad5783c61466aa5eb
austinxduong <austinxduong@gmail.com>
2024-06-07 14:52:18 -0700

    count words str

---
+++ Strings/CountWordStr.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class CountWordStr {
+    public static int strCount(String s) {
+        String[] wordStrings = s.split("\\s+");
+        return wordStrings.length;
+    }
+
+    public static void main(String[] args) {
+        String s = "Woah it's almost Summer! & that means more fun times and moments, yay!;)";
+        System.out.println(strCount(s));
+    }
+}
