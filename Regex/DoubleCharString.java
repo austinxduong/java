fbf0b36ceab4cd8e5baf717239c2354b8c408fb0
austinxduong <austinxduong@gmail.com>
2024-06-14 14:04:32 -0700

    regex double char string

---
+++ Regex/DoubleCharString.java
@@ -0,0 +1,21 @@
+package Regex;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class DoubleCharString {
+    public static boolean dCharS(String word) {
+        Pattern pat = Pattern.compile("");
+        Matcher matcher = pat.matcher(word);
+
+        if (matcher.find()) {
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        String word = "weed";
+        System.out.println(dCharS(word));
+    }
+}
+
+// true;
