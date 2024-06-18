09834eef464c197a9cd695840ce9bf0791fd8532
austinxduong <austinxduong@gmail.com>
2024-06-18 13:44:17 -0700

    maskify regex

--- Regex/Maskify.java
+++ Regex/Maskify.java
@@ -2,19 +2,25 @@ package Regex;
 
 public class Maskify {
     public static String maskifyS(String s) {
-		String sx = s.substring(s.length() - 4);
-		String l4 = s.substring(0, s.length() - 4);
-		String maskify = l4.replaceAll( "\\d|\\w|\\D", "#");
-		
-		return maskify + sx;
+
+        if (s.length() <= 3) {
+            return s;
+
+        } else {
+            String sx = s.substring(s.length() - 4);
+            String l4 = s.substring(0, s.length() - 4);
+            String maskify = l4.replaceAll( "\\d|\\w|\\D", "#");
+            return maskify + sx;
+        }
     }
 
     public static void main(String[] args) {
-        String s = "9879803-352435-35635478";
+        String s = "1234";
         System.out.println(maskifyS(s));
     }
 }
-
+// 1234 // 1234
+// 12345 // #2345
 // 574839374950 // ########4950
 // 000-000-000 // #######-000
 // 9879803-352435-35635478 // ###################5478
