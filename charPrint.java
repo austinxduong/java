8c52adca8dc46fe7619cce2f165934c856317bff
austinxduong <austinxduong@gmail.com>
2024-06-28 15:37:09 -0700

    char

---
+++ charPrint.java
@@ -0,0 +1,15 @@
+public class charPrint {
+    public static char charPrinting(char charr) {
+        char c1 = 'A';
+        char m3 = '!';
+        char m1 = '\u0021';
+        char c2 = 65;
+        char c3 = '\u0041';
+        return c2;
+    }
+
+    public static void main(String[] args) {
+        char m3 = '\u0021';
+        System.out.println(m3);
+    }
+}
