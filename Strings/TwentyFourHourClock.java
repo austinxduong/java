993979a2b40c2c030ffa620cfe210c95e23cc6c0
austinxduong <austinxduong@gmail.com>
2024-07-07 15:22:55 -0700

    24 hour clock from secs

--- Strings/TwentyFourHourClock.java
+++ Strings/TwentyFourHourClock.java
@@ -2,7 +2,8 @@ package Strings;
 
 public class TwentyFourHourClock{
     public static String clock(int seconds) {
-        int hours = seconds / 3600;
+        int hours = (seconds / 3600) % 24;
+        // int hours = seconds / 3600;
         int minutes = (seconds % 3600) / 60;
         seconds = seconds % 60;
 
@@ -14,7 +15,7 @@ public class TwentyFourHourClock{
         System.out.println(clock(seconds));
     }
 }
-//87000 // 24:10:00
+// 87000 // 24:10:00
 // 8700 // 02:25:00 
 // 870 // 00:14:30
 
