44eee7bb7324503048e8b5d7f4e55ade163c7435
austinxduong <austinxduong@gmail.com>
2024-07-09 13:19:02 -0700

    loop multidimensional array alphabet occurence

--- Arrays/MultidimensionalArray.java
+++ Arrays/MultidimensionalArray.java
@@ -1,21 +1,34 @@
 package Arrays;
 
-import java.util.Arrays;
+
 
 public class MultidimensionalArray {
-    public static char[][] alphabetCounter(char[][] arr, char c) {
+        public static int alphabetCounter(char[][] arr, char c) {
+        int total = 0;
+        String charArray = "";
+
         for (int d = 0; d < arr.length; d++) {
             for (int e = 0; e < arr[d].length; e++) { 
+                charArray += arr[d][e];
+                }
             }
-        }
-        return arr;
+        for(int i = 0; i < charArray.length(); i++) {
+            if (charArray.charAt(i) == c) {
+                total++;
+                }
+            }
+            return total;
     }
 
     public static void main(String[] args) {
-        char[][] arr = { {'A', 'B', 'C', 'D'}, {'D', 'E', 'F', 'G'}};
+        char[][] arr = { {'A', 'D', 'D', 'D'}, {'D', 'D', 'F', 'G'} };
         char c = 'D';
-        System.out.println(Arrays.deepToString(alphabetCounter(arr, c)));
+        System.out.println(alphabetCounter(arr, c));
     }
 }
 
-//[[A, B, C, D], [D, E, F, G]]
+// { {'A', 'B', 'C', 'D'}, {'D', 'E', 'F', 'G'} } // 2
+// { {'A', 'B', 'C', 'D'}, {'D', 'D', 'F', 'G'} } // 3
+// { {'A', 'D', 'C', 'D'}, {'D', 'D', 'F', 'G'} } // 4
+// { {'A', 'D', 'D', 'D'}, {'D', 'D', 'F', 'G'} } // 5
+
---
+++ Arrays/MultidimensionalArrayLoop.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MultidimensionalArrayLoop {
+    public static char[][] alphabetCounter(char[][] arr, char c) {
+        for (int d = 0; d < arr.length; d++) {
+            for (int e = 0; e < arr[d].length; e++) { 
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        char[][] arr = { {'A', 'B', 'C', 'D'}, {'D', 'E', 'F', 'G'}};
+        char c = 'D';
+        System.out.println(Arrays.deepToString(alphabetCounter(arr, c)));
+    }
+}
+
+//[[A, B, C, D], [D, E, F, G]]
