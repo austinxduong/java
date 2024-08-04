7815444e2814f47a0a90f81aa11d39e40416bbb9
austinxduong <austinxduong@gmail.com>
2024-08-04 15:33:30 -0700

    array multiplier algo

--- ArrayList/ArrayMultiplier.java
+++ ArrayList/ArrayMultiplier.java
@@ -1,7 +1,6 @@
 package ArrayList;
 
 import java.util.Arrays;
-// import java.util.ArrayList;
 
 public class ArrayMultiplier {
     public static Object[][] arrayMultiplier(Object[] items) {
@@ -24,31 +23,4 @@ public class ArrayMultiplier {
     }
 }
 
-/*
-expected output: 
-[["a", "a", "a", "a"], [8, 8, 8, 8], ["d", "d", "d", "d"], ["yh", "yh", "yh", "yh"]]
- */
-
-// {"a", 8, "d", 9, "y"} // [[null, y, null, null, null], [null, null, null, null, null], [null, null, null, null, null], [null, null, null, null, null], [null, null, null, null, null]]
-
-
-/*
-for (Integer i = 0; i < newItems.length; i++) {
-    for (Integer j = 0; j < newItems[i].length; j++) {
-        newItems[i][j] = 4;
-        // [[4, 4, 4, 4, 4], [4, 4, 4, 4, 4], [4, 4, 4, 4, 4], [4, 4, 4, 4, 4], [4, 4, 4, 4, 4]]
-    }
-}
- */
-
-//  for (Object e : items) {
-//     for (Integer i = 0; i < newItems.length; i++) {
-//         for (Integer j = 0; j < newItems[i].length; j++) {
-//             newItems[i][j] = e;
-//             // [[yh, yh, yh, yh, yh], [yh, yh, yh, yh, yh], [yh, yh, yh, yh, yh], [yh, yh, yh, yh, yh], [yh, yh, yh, yh, yh]]
-//         }
-//     }
-// }
-
-
-//[["a", "a", "a", "a"], [8, 8, 8, 8], ["d", "d", "d", "d"], ["yh", "yh", "yh", "yh"]]
+// {"a", 8, "d", 9, "yh"} // [["a", "a", "a", "a"], [8, 8, 8, 8], ["d", "d", "d", "d"], ["yh", "yh", "yh", "yh"]]
