e66685c9a6c41f8bd29788a48a97ff84b7353f4c
austinxduong <austinxduong@gmail.com>
2024-08-06 14:28:41 -0700

    integer object array work in progress arraylist

--- IntegerObj.java
+++ IntegerObj.java
@@ -1,29 +1,45 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 
 public class IntegerObj {
-    public static Object[] integerObj(Object[] arr) {
+    public static ArrayList<Object> integerObj(Object[] arr) {
+        ArrayList<Object> objArrayList = new ArrayList<Object>();   
+
         Integer intLength = 0;
         Object[] integerObjArray = new Object[arr.length - intLength]; // 5 - 2
+        // Object[] integerObjArray2 = new Object[arr.length - intLength];
     
         for (Object intInc : arr) {
             if (intInc instanceof Integer) {
                 intLength++;
                 // output: 2
             }
+        }
            
         for (Integer i = 0; i < arr.length; i++) {
             if (arr[i] instanceof Integer) {
                 integerObjArray[i] = arr[i];
                 }
             }
-        }
-        return integerObjArray;
+
+        for (Integer i = 0; i < integerObjArray.length; i++) {
+            if (integerObjArray[i] != null) {
+                objArrayList.add(integerObjArray[i]);
+                // {"hello", 81, "basketball", 1234, "fox"} 
+                // [81, 1234]
+                }
+            }
+        
+
+
+        return objArrayList;
         //[null, 81, null, 1234, null]
         // integer vaules are added to the object array, however null values are still added
     }
 
     public static void main(String[] args) {
         Object[] arr = {"hello", 81, "basketball", 1234, "fox"};
-        System.out.println(Arrays.toString(integerObj(arr)));
+        System.out.println(integerObj(arr));
     }
 }
+
