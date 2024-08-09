7d10041b43ad21463d90716c4d345384ffdd0931
austinxduong <austinxduong@gmail.com>
2024-08-09 13:50:01 -0700

    binary search integer array

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static Integer binarySearch(int[] sortedArray, int l, int r, int elem) {
+        Integer index = 0;
+
+        while (l <= r) {
+            Integer middle = ((l + r) / 2);
+
+            if (sortedArray[middle] < elem) {
+                l = middle + 1;
+            } else if (sortedArray[middle] > elem) {
+                r = middle - 1;
+            } else if (sortedArray[middle] == elem) {
+                index = elem;
+                break;
+            }
+        }
+        return index;
+    }
+
+    
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        Integer l = 0;
+        Integer r = 9;
+        int elem = 7;
+
+        System.out.println(binarySearch(sortedArray, l, r, elem));
+    }
+}
+
+// {1, 2, 3, 4, 5, 6, 7, 8, 9, 10} // 7
