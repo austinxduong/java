2554a9f9862fa51d0cbbe0b47f0fff167db94134
austinxduong <austinxduong@gmail.com>
2024-08-17 14:01:51 -0700

    string encryption linkedhashmap string algo work in progress

--- LinkedHashMap/StringEncryption.java
+++ LinkedHashMap/StringEncryption.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Arrays;
 
 public class StringEncryption {
-    public static String[] stringEncryption(String word) {
+    public static String stringEncryption(String word) {
 
         LinkedHashMap<String, String> linkedHashMapEncryption = new LinkedHashMap<String, String>();
         char[] arrayStr = word.toCharArray();
@@ -14,41 +14,50 @@ public class StringEncryption {
         String[] rString = randomString.split("");
         // char[] revCharStr = reversedString.toCharArray();
         
-        // String wrd = "aca"; 
+        String wrd = "aca"; 
         // String encryptString = "";
         
 
-        // linkedHashMapEncryption.put("a", "0");
-        // linkedHashMapEncryption.put("e", "1");
-        // linkedHashMapEncryption.put("i", "2");
-        // linkedHashMapEncryption.put("o", "2");
-        // linkedHashMapEncryption.put("u", "2");
+        linkedHashMapEncryption.put("a", "0");
+        linkedHashMapEncryption.put("e", "1");
+        linkedHashMapEncryption.put("i", "2");
+        linkedHashMapEncryption.put("o", "2");
+        linkedHashMapEncryption.put("u", "2");
 
-        String reversedString = "";
+        String reversedString = ""; //elppA
+        
 
         for (Integer i = arrayStr.length - 1; i >= 0; i--) {
-            
             reversedString += arrayStr[i];
         }
 
-        String[] revStrArray = reversedString.split(""); // [e, l, p, p, A]
-        
+        // String[] strArray = word.split("");
+        String[] revStrArray = reversedString.split(""); // [e, l, p, p, a]
+        // String[] newRevStrArray = new String[revStrArray.length];
+        // String wordArray = revStrArray.toString();
 
         // for (Integer i = 0; i < reversedString.length(); i++) {
-            // for (Integer i = 0; i  < revStrArray.length; i++) {
-            //     for (Map.Entry<String, String> entry : linkedHashMapEncryption.entrySet()) {
-            //         if (linkedHashMapEncryption.containsKey(revStrArray[i])) {
-            //             reversedString.replace("reversedString[i]", entry.getValue());
-            //         }
-            //     }
+            
+            for (Integer d = 0; d < revStrArray.length; d++) {
+                for (Map.Entry<String, String> entry : linkedHashMapEncryption.entrySet()) {
+                    if (entry.getKey().equals(revStrArray[d])) {
+                        
+                        revStrArray[d] = entry.getValue(); // [a, p, p, l, 1]
+                    
+                        // revStrArray[d] += entry.getValue(); // [e0, l, p, p, a]
+                        // revStrArray[d].replace("revStrArray[d]", entry.getValue());
+                    }
+                }
+            }
+            String wordArray = String.join("", revStrArray);
 
-            // }
-        
         // }
-        return revStrArray; // [e, l, p, p, A]
+        return wordArray; // 1lpp0
+        // return revStrArray; // [1, l, p, p, 0]
+        // expected output: 1lpp0aca
     }   
     public static void main(String[] args) {
-        String word = "Apple";
-        System.out.println(Arrays.toString(stringEncryption(word)));
+        String word = "apple";
+        System.out.println(stringEncryption(word));
     }
 }
