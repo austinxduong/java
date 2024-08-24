b5770cc21e4ef34e98aa6d10e5febc73a029e2da
austinxduong <austinxduong@gmail.com>
2024-08-24 14:23:15 -0700

    omnipresent integer array/arraylist algo work in prog

--- OmnipresentInteger.java
+++ OmnipresentInteger.java
@@ -3,21 +3,20 @@ import java.util.ArrayList;
 
 
 public class OmnipresentInteger {
-    public static ArrayList<ArrayList<Integer>> omnipresentInteger(int[][] array, int integerr) {
-        ArrayList<ArrayList<Integer>> arrayListInteger = new ArrayList<ArrayList<Integer>>();
+    public static ArrayList<Integer> omnipresentInteger(int[][] array, int integerr) {
+
+        ArrayList<Integer> integerArrayList = new ArrayList<Integer>();
 
         for (int i = 0; i < array.length; i++) {
-            ArrayList<Integer> subArrays = new ArrayList<Integer>();
-
             for (int a = 0; a < array[i].length; a++) {
-                subArrays.add(array[i][a]);
+                if (array[i][a] == integerr) {
+                    integerArrayList.add(array[i][a]);
+                }
             }
-            arrayListInteger.add(subArrays);
+        }
+        return integerArrayList; // [3, 3, 3, 3, 3, 3]
     }
 
-
-        return arrayListInteger; // [[3, 4], [8, 3, 2], [3], [9, 3], [5, 3], [4, 3]] // multidimensional array
-    }
 
     public static void main(String[] args) {
         int[][] array = {{3, 4}, {8, 3, 2}, {3}, {9, 3}, {5, 3}, {4, 3}};
@@ -26,4 +25,27 @@ public class OmnipresentInteger {
     }
 }
 
-                    
+// ArrayList<ArrayList<Integer>> arrayListInteger = new ArrayList<ArrayList<Integer>>();
+
+// for (int i = 0; i < array.length; i++) {
+//     ArrayList<Integer> subArrays = new ArrayList<Integer>();
+
+//     for (int a = 0; a < array[i].length; a++) {
+//         subArrays.add(array[i][a]);
+//     }
+//     arrayListInteger.add(subArrays);
+// }
+
+// ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+// for (int i = 0; i < arrayListInteger.size(); i++) {
+// for (int a = 0; a < arrayListInteger.get(i).size(); a++) {
+
+    
+
+//     // arrayList.add(arrayListInteger[i][a]);
+//     }
+// return arrayList; // [[3, 4], [8, 3, 2], [3], [9, 3], [5, 3], [4, 3]] // multidimensional array
+
+//  }
+// }
