759942a57552453ffd28ba58450700e13d634d0b
austinxduong <austinxduong@gmail.com>
2024-08-30 13:18:22 -0700

    omnipresent integer arraylist hashset algo extended com

--- ArrayList/OmnipresentIntegerV0.java
+++ ArrayList/OmnipresentIntegerV0.java
@@ -1,18 +1,13 @@
 package ArrayList;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.LinkedHashSet;
-import java.util.Arrays;
 
 
 public class OmnipresentIntegerV0 {
-    public static int omnipresentInteger(int[][] array, int integerr) {
+    public static boolean omnipresentInteger(int[][] array, int integerr) {
 
         ArrayList<LinkedHashSet<Integer>> integerArrayList = new ArrayList<LinkedHashSet<Integer>>();
-        ArrayList<Integer[]> arrayListYay = new ArrayList<Integer[]>();
-        int[][] arrayInteger = new int[integerArrayList.size()][];
-        Integer[] sArrayInteger = new Integer[13];
 
 
         for (int i = 0; i < array.length; i++) {
@@ -25,79 +20,42 @@ public class OmnipresentIntegerV0 {
                    
                 }
                 integerArrayList.add(subArray);
-
-                Integer[] subArrayInteger = subArray.toArray(new Integer[0]);
-                // Integer[] sArrayInteger = new Integer[10];
-
-               arrayListYay.add(sArrayInteger);
-                for (int r = 0; r < subArrayInteger.length; r++) {
-                    sArrayInteger[i] = subArrayInteger[r];
-                }
             }
-
             int increment = 0;
-            for (int g = 0; g < arrayListYay.size(); g++) {
-                for (int w = 0; w < arrayListYay.get(g).length; w++) {
-                    if (arrayListYay.get(g)[w] == integerr) {
-                        increment++;
-                }
+
+        for (int d = 0; d < integerArrayList.size(); d++) {
+           
+                if (integerArrayList.get(d).contains(integerr)) {
+                    increment++;
                 
-
-                }
             }
-        //     for (int r = 0; r < subArrayInteger.length; r++) {
-        //         sArrayInteger[i] = subArrayInteger[r];
-        //     }
-        // }
-        return increment;
+        }
+
+        if (increment < integerArrayList.size()) {
+            return false;
+        }
+            
+        return true; 
+
     }
-                // for (int d = 0; d < integerArrayList.size(); d++) {
-                //  for (int e = 0; e < integerArrayList.size(); e++) {
-                //     integerArrayList.get(d).get(e);
-                //     }
-                // }
-            
-
-        
-        // int[][] arrayInteger = new int[integerArrayList.size()][];
-
-        // for (int d = 0; d < integerArrayList.size(); d++) {
-        //     subArray.toArray();
-        //     arrayInteger[i] = 
-        //     }
-            
-    //     int arraySize = array.length; // 6
-
-    //     if (integerArrayList.size() < arraySize) {
-    //         return false;
-    //     }
-    // }
-
-// ArrayList<Integer> arrayList = new ArrayList<Integer>();
-
-// }
-//         int[][] arrayInteger = new int[integerArrayList.size()][];
-
-
-//         for (int d = 0; d < integerArrayList.size(); d++) {
-//             arrayInteger[i] = integerArrayList.get(d);
-//         }
-
-
-        
-        // return true; // [3, 3, 3, 3, 3, 3]
-        ;
-//         }
-// }
-
 
 
     public static void main(String[] args) {
-        int[][] array = {{3, 4}, {8, 3, 3, 2}, {}, {9, 3}, {5, 3}, {4, 3}};
+        int[][] array = {{3, 4, 3, 4}, {8, 3, 2}, {}, {9, 3}, {5, 3}, {4, 3}, {3, 4}, {5, 3}};
         int integerr = 3;
         System.out.println(omnipresentInteger(array, integerr));
     }
 }
+
+
+// {{3, 3, 4}, {8, 3, 3, 2}, {}, {3}, {3}, {9, 3}, {5, 3}, {4, 3}}, 3 // false
+// {{3, 3, 4}, {8, 3, 3, 2}, {}, {}, {}, {9, 3}, {5, 3}, {4, 3}}, 3 // false
+// {{3, 3, 4}, {8, 3, 3, 2}, {3}, {3}, {3}, {9, 3}, {5, 3}, {4, 3}}, 3 // true
+// {{1, 1}, {1, 3}, {5, 1}, {6, 1}} // false
+// {{3, 4, 3, 4}, {8, 3, 2}, {}, {9, 3}, {5, 3}, {4, 3}, {3, 4}, {5, 3}} // false
+
+
+
 
 
 // {{3, 4}, {8, 3, 2}, {}, {9, 3}, {5, 3}, {4, 3} // false
