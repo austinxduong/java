26ec798f07e2fa1b82bab9cd48623057d66a01a4
austinxduong <austinxduong@gmail.com>
2024-09-03 13:53:07 -0700

    index filtering string algo work in prog

--- LinkedHashMap/IndexFilteringString.java
+++ LinkedHashMap/IndexFilteringString.java
@@ -5,23 +5,70 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class IndexFilteringString {
-    public static int[] indexFilteringString(String words, int[] indexes) {
+    public static char[] indexFilteringString(String words, int[] indexes) {
         String[] wordsSplit = words.split("");
         int[] integerArray = new int[wordsSplit.length];
 
-        for (int i = 0; i < wordsSplit.length; i++) {
-            integerArray[i] += words.indexOf(wordsSplit[i]);
-        }
-        return integerArray;
+        int incr = 0;
+        String str = "";
+        char[] charArray = new char[40];
+        char[] charArrayString = words.toCharArray();
+        String[] stringArray = new String[40];
         
+        
+
+        // for (int i = 0; i < words.length(); i++) {
+            // for (int a = 0; a < indexes.length; a++) {
+                // if (words.indexOf(i) == ) {
+                    // charArray[i] += words.charAt(i);
+                    // incr += indexes[a];
+                
+                
+            // }
+
+
+
+
+            
+
+            for (int i = 0; i < charArrayString.length; i++) {
+                // for (int a = 0; a < indexes.length; a++) {
+
+                    // int found = new String(charArrayString).indexOf(indexes[a]);
+                    
+                    if (charArrayString[i] == 'a') {
+                        charArray[i] += charArrayString[i];
+       
+                    }
+                // }
+                // charArrayString[i] += words.charAt(i); 
+                // Mountains of ice cream and cookies
+            }
+
+            // for (int i = 0; i < charArrayString.length; i++) {
+            //     str += words.charAt(i); 
+            //     // Mountains of ice cream and cookies
+            // }
+        
+
+        
+
+        // for (int i = 0; i < wordsSplit.length; i++) {
+        //     integerArray[i] += words.indexOf(wordsSplit[i]);
+        //     // [0, 1, 2, 3, 4, 5, 6, 3, 8, 9, 1, 11, 9, 6, 14, 15, 9, 14, 18, 15, 5, 21, 9, 5, 3, 25, 9, 14, 1, 1, 30, 6, 15, 8]
+        // }
+        return charArray; 
+    
     }
 
 
 
     public static void main(String[] args) {
-        String words = "Mountains of ice cream and cookies";
+        String words = "Mountains of ice cream and cookies"; // 34 letters
         int[] indexes = {9, 12, 16, 18, 25, -2};
         System.out.println(Arrays.toString(indexFilteringString(words, indexes)));
+        // System.out.println(indexFilteringString(words, indexes));
+
     }
      
 }
