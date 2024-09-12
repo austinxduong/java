accdc8d7dc78e88817e15a9944f82990a47c168f
austinxduong <austinxduong@gmail.com>
2024-09-12 15:42:32 -0700

    ordinal number linkedhashset algo work in prog

--- LinkedHashMap/OrdinalNumber.java
+++ LinkedHashMap/OrdinalNumber.java
@@ -2,30 +2,31 @@ package LinkedHashMap;
 
 import java.util.LinkedHashMap;
 import java.util.Arrays;
+import java.util.Map;
 
 public class OrdinalNumber {
     public static String ordinalNumber(int integer) {
 
-        LinkedHashMap<Integer, String> linkedHashMap = new  LinkedHashMap<Integer, String>();
-        linkedHashMap.put(1, "ST");
-        linkedHashMap.put(2, "ND");
-        linkedHashMap.put(3, "RD");
-        linkedHashMap.put(4, "TH");
-        linkedHashMap.put(5, "TH");
-        linkedHashMap.put(6, "TH");
-        linkedHashMap.put(7, "TH");
-        linkedHashMap.put(8, "TH");
-        linkedHashMap.put(9, "TH");
-        linkedHashMap.put(10, "TH");
-        linkedHashMap.put(11, "TH");
-        linkedHashMap.put(12,"TH");
-        linkedHashMap.put(13, "TH");
-        linkedHashMap.put(14, "TH");
-        linkedHashMap.put(15, "TH");
-        linkedHashMap.put(16, "TH");
-        linkedHashMap.put(17, "TH");
-        linkedHashMap.put(18, "TH");
-        linkedHashMap.put(19, "TH");
+        LinkedHashMap<String, String> linkedHashMap = new  LinkedHashMap<String, String>();
+        linkedHashMap.put("1", "ST");
+        linkedHashMap.put("2", "ND");
+        linkedHashMap.put("3", "RD");
+        linkedHashMap.put("4", "TH");
+        linkedHashMap.put("5", "TH");
+        linkedHashMap.put("6", "TH");
+        linkedHashMap.put("7", "TH");
+        linkedHashMap.put("8", "TH");
+        linkedHashMap.put("9", "TH");
+        linkedHashMap.put("10", "TH");
+        linkedHashMap.put("11", "TH");
+        linkedHashMap.put("12","TH");
+        linkedHashMap.put("13", "TH");
+        linkedHashMap.put("14", "TH");
+        linkedHashMap.put("15", "TH");
+        linkedHashMap.put("16", "TH");
+        linkedHashMap.put("17", "TH");
+        linkedHashMap.put("18", "TH");
+        linkedHashMap.put("19", "TH");
         
         
         String[] sIntegerArray = String.valueOf(integer).split(""); // [5, 5, 3]
@@ -35,6 +36,7 @@ public class OrdinalNumber {
         String[] subStringSlice = new String[sInteger.length()];
         String[] subStringToArray = subString.split("");
         String subStringInteger = "";
+        String linkedSubStringInteger = "";
 
         // String[] digits = new String[2];
 
@@ -46,13 +48,25 @@ public class OrdinalNumber {
                 }            
             }
 
-        return subStringInteger; // "18"
+        for (Map.Entry<String, String> entry : linkedHashMap.entrySet()) {
+            if (entry.getKey().equals(subStringInteger)) {
+                linkedSubStringInteger = entry.getValue();
+                    }
+            
+        }
+    
+
+        return linkedSubStringInteger; // "ST"
         
     }
 
     public static void main(String[] args) {
-        int integer = 528;
+        int integer = 521;
         System.out.println(ordinalNumber(integer));
-        // System.out.println(Arrays.toString(ordinalNumber(integer)));
     }
 }
+
+// 521 // "ST"
+// 523 // "RD"
+// 522 // "ND"
+// 531 // "ST"
