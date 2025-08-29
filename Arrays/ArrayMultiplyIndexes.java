c16f0b0b60ce5a49350f59e131770a46d26bb986
austinxduong <austinxduong@gmail.com>
2025-08-29 16:19:25 -0700

    java not compiling, getting java.lang.ClassNotFoundException & Error: Could not find or load main clas

---
+++ Arrays/ArrayMultiplyIndexes.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class ArrayMultiplyIndexes {
+    public static int[] arrayOfMultiples (int num int length) {
+
+    }
+}
+
+public static void main(String[] args) {
+int[] arraySet = [1, 2, 3, 4, 5, 6, 7]
+System.out.println("printed multiples of array elements")
+    }
+}
--- Arrays/ArraySort.java
+++ Arrays/ArraySort.java
@@ -11,7 +11,7 @@ public class ArraySort {
 public static void main(String[] args){
     int[] arrays = {-14, 0, 1, 13, 32, 5, 8, 10, 55, 100, 235};
     System.out.println("Sorted Arrays Asc:" + " "  +Arrays.toString(ArraySortAsc(arrays)));
-}
+    }
 }
 
 // [-14, 0, 1, 5, 8, 10, 13, 32, 55, 100, 235]
