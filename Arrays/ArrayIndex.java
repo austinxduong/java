b8dd4a00f1b47c9162bf6caac737abd62b81215f
austinxduong <austinxduong@gmail.com>
2025-11-19 16:59:16 -0800

    java now succesfully compiles

--- Arrays/ArrayIndex.java
+++ Arrays/ArrayIndex.java
@@ -5,4 +5,8 @@ public class ArrayIndex {
         int index = (int) i;
         return array[index];
     }
+    public static void main(String[] args) {
+    int[] numbers = {10, 20, 30, 40};
+    System.out.println(indexAt(numbers, 2.7)); // Should print 30
+}
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 a compilation of java algorithms & data structures
+
+note to self
+to compile/run:
+```
+javac Arrays/filenameHere.java
+java Arrays.filename
+
+```
